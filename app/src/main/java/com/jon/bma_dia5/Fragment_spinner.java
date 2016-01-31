0ec68ec98bf4a185b251944730d77c37dff58d05
godbhaal <godbhaal666@gmail.com>
2016-01-31 18:00:32 +0100

    res

--- app/src/main/java/com/jon/bma_dia5/Fragment_spinner.java
+++ app/src/main/java/com/jon/bma_dia5/Fragment_spinner.java
@@ -1,33 +1,26 @@
 package com.jon.bma_dia5;
 
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 /**
  * Created by jon on 29/01/16.
  */
-public class Fragment2 extends Fragment {
+public class Fragment_spinner extends Fragment {
 
     private View view;
     private Spinner spinner;
-    Boolean st;
-    SharedPreferences sharedPref;
-    TextView textView;
 
-    public static Fragment2 newInstance(){
+    public static Fragment_spinner newInstance(){
         Bundle args = new Bundle();
-        Fragment2 fragment = new Fragment2();
+        Fragment_spinner fragment = new Fragment_spinner();
         fragment.setArguments(args);
         return fragment;
     }
@@ -36,7 +29,6 @@ public class Fragment2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.layout_fragment2, container, false);
-        sharedPref = PreferenceManager.getDefaultSharedPreferences(getContext());
         return view;
     }
 
@@ -46,13 +38,6 @@ public class Fragment2 extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         spinner = (Spinner) view.findViewById(R.id.spinner);
         spinner.setAdapter(new MySpinnerAdapter(view.getContext()));
-        textView = (TextView) getActivity().findViewById(R.id.textView3);
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        st = sharedPref.getBoolean("pref_1", true);
-        textView.setText(st.toString());
-    }
 }
