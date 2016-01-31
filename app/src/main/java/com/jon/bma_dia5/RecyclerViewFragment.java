ba06d3616679d5b061608c9fee2f7ead31f1f5ff
godbhaal <godbhaal666@gmail.com>
2016-02-01 00:23:39 +0100

    RecyclerView added

---
+++ app/src/main/java/com/jon/bma_dia5/RecyclerViewFragment.java
@@ -0,0 +1,75 @@
+package com.jon.bma_dia5;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+public class RecyclerViewFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private static String LOG_TAG = "RecyclerViewActivity";
+
+    public static RecyclerViewFragment newInstance() {
+        Bundle args = new Bundle();
+        RecyclerViewFragment fragment = new RecyclerViewFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_recycler_view, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mRecyclerView = (RecyclerView) getActivity().findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new MyRecyclerViewAdapter(getDataSet());
+        mRecyclerView.setAdapter(mAdapter);
+    }
+    // Code to Add an item with default animation
+    //((MyRecyclerViewAdapter) mAdapter).addItem(obj, index);
+
+    // Code to remove an item with default animation
+    //((MyRecyclerViewAdapter) mAdapter).deleteItem(index);
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((MyRecyclerViewAdapter) mAdapter).setOnItemClickListener(new MyRecyclerViewAdapter.MyClickListener() {
+            @Override
+            public void onItemClick(int position, View v) {
+                Log.i(LOG_TAG, " Clicked on Item " + position);
+            }
+        });
+    }
+
+    private ArrayList<DataObject> getDataSet() {
+        // AJUNTA tituls i valors
+        String[] titles, values;
+        ArrayList results = new ArrayList<DataObject>();
+        titles = getResources().getStringArray(R.array.recycler_titles);
+        values = getResources().getStringArray(R.array.recycler_values);
+        for (int c = 0; c < titles.length; c++) {
+            DataObject obj = new DataObject(titles[c], values[c]);
+            results.add(obj);
+        }
+        return results;
+    }
+}
